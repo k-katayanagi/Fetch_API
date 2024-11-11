947a6056d21beed0fa9f369c3e77fd7c892fe057
k-katayanagi <kkatayanagi.ka@gmail.com>
2024-11-11 15:37:09 +0900

    Initial commit

---
+++ js/fetch_crud.js
@@ -0,0 +1,4 @@
+const postIkku = document.querySelector('.postIkku');
+const postBtn = document.querySelector('.postBtn');
+const ikkulist = document.querySelector('.ikkuList');
+const url = 'http://localhost:3000/ikku';
